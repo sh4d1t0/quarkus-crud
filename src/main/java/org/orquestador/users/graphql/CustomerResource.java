d85e675a611e5d677bf9cd82c5342fcca60e5dc2
Shadito <shadito@live.com.mx>
2023-05-04 13:15:57 -0600

    Add GraphQL

---
+++ src/main/java/org/orquestador/users/graphql/CustomerResource.java
@@ -0,0 +1,22 @@
+package org.orquestador.users.graphql;
+
+import jakarta.inject.Inject;
+import org.eclipse.microprofile.graphql.Description;
+import org.eclipse.microprofile.graphql.GraphQLApi;
+import org.eclipse.microprofile.graphql.Query;
+import org.orquestador.users.entities.Users;
+import org.orquestador.users.repositories.UserRepository;
+
+import java.util.List;
+
+@GraphQLApi
+public class CustomerResource {
+    @Inject
+    UserRepository userRepository;
+
+    @Query("allUsers")
+    @Description("List all users")
+    public List<Users> getAllUsers() {
+        return userRepository.listAll();
+    }
+}
