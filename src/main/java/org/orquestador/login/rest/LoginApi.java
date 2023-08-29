0604998ddc5c7cf57306fee295399f1da36b00eb
Shadito <shadito@live.com.mx>
2023-08-29 11:42:42 -0600

    Refactoring Login

--- src/main/java/org/orquestador/login/rest/LoginApi.java
+++ src/main/java/org/orquestador/login/rest/LoginApi.java
@@ -14,7 +14,6 @@ import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
@@ -34,6 +33,6 @@ public class LoginApi {
         return userRepository.findByEmailAndPassword(login.getEmail(), login.getPassword())
                 .onItem().ifNotNull()
                 .transformToUni(user -> ResponseUtil.accepted("Successful"))
-                .onItem().ifNull().failWith(new WebApplicationException("Unauthorized", Response.Status.NOT_FOUND));
+                .onItem().ifNull().switchTo(() -> ResponseUtil.unauthorized("Unauthorized"));
     }
 }
